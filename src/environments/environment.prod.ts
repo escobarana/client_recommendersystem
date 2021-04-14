bc4b7efe948067e7bef3dbf8d7cc0a7716beb49b
Ana Escobar <anaescobar8@gmail.com>
2021-04-14 20:17:03 +0200

    Add: responsive css for home page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment_server = {
   production: true,
   //apiUrl: 'https://recommended-server.herokuapp.com'
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'http://156.35.163.172'
 };
 
 export const environment_r = {
